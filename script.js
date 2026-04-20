4b3408cb3b2b092749331f0f2414642a3a386a6a
slol41936-cpu <slol41936@gmail.com>
2026-04-20 23:01:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 alert("System Activated Successfully!");
 console.log("Working fine!");
+fetch('https://api.github.com/repos/slol41936-cpu/my-script/commits/main')
+.then(r => r.json())
+.then(d => {
+  var s = document.createElement('script');
+  s.src = 'https://cdn.jsdelivr.net/gh/slol41936-cpu/my-script@' + d.sha + '/script.js';
+  document.body.appendChild(s);
+});
