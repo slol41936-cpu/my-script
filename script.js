8f31be252977ce63ff1c3554ad0b5fef4a8bb8ae
slol41936-cpu <slol41936@gmail.com>
2026-05-11 15:42:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,134 +1,121 @@
-/* আপনার বন্ধুর অরিজিনাল কোড - ১০০% কমপ্লিট ভার্সন */
-(function () {
+/* আপনার বন্ধুর হুবহু অরিজিনাল জটিল কোড - আপনার তথ্য দিয়ে ফিক্স করা */
+
+function a0_0x1703(_0x24082f, _0x44ab98) {
+  _0x24082f = _0x24082f - 0x196;
+  const _0x4cfb7e = a0_0x4cfb();
+  let _0x170385 = _0x4cfb7e[_0x24082f];
+  if (a0_0x1703["bPtxJv"] === undefined) {
+    var _0x321e82 = function (_0x1542a8) {
+      const _0x502196 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=";
+      let _0x33429b = "", _0x115a87 = "";
+      for (let _0x1aa4f7 = 0x0, _0x206f72, _0x12a38d, _0x504c77 = 0x0; (_0x12a38d = _0x1542a8["charAt"](_0x504c77++)); ~_0x12a38d && ((_0x206f72 = _0x1aa4f7 % 0x4 ? _0x206f72 * 0x40 + _0x12a38d : _0x12a38d), _0x1aa4f7++ % 0x4) ? (_0x33429b += String["fromCharCode"](0xff & (_0x206f72 >> ((-0x2 * _0x1aa4f7) & 0x6)))) : 0x0) {
+        _0x12a38d = _0x502196["indexOf"](_0x12a38d);
+      }
+      for (let _0x3811dd = 0x0, _0x4820aa = _0x33429b["length"]; _0x3811dd < _0x4820aa; _0x3811dd++) {
+        _0x115a87 += "%" + ("00" + _0x33429b["charCodeAt"](_0x3811dd)["toString"](0x10))["slice"](-0x2);
+      }
+      return decodeURIComponent(_0x115a87);
+    };
+    ((a0_0x1703["LbHxtW"] = _0x321e82), (a0_0x1703["eflYMB"] = {}), (a0_0x1703["bPtxJv"] = !![]));
+  }
+  const _0x22a121 = _0x4cfb7e[0x0], _0x1df100 = _0x24082f + _0x22a121, _0x353ae0 = a0_0x1703["eflYMB"][_0x1df100];
+  return (!_0x353ae0 ? ((_0x170385 = a0_0x1703["LbHxtW"](_0x170385)), (a0_0x1703["eflYMB"][_0x1df100] = _0x170385)) : (_0x170385 = _0x353ae0), _0x170385);
+}
+
+function a0_0x4cfb() {
+  const _0x3301e1 = ["qNf5DKW", "BfPUt0u", "C3rpEhe", "B25SB2fK", "y3vYCMvUDfrPBwu", "D2fSBgv0vxnLCKLK", "u1PnDwG", "qwnJzxnZigrLBMLLza", "t0Hot2e", "uMLWq2C", "AKnXqK8", "mty1mtyZowXft0XLDW", "Ahr0Chm6lY93D3CUz3n0yxrPyY5JB20VzMLYzwjHC2vQCY85lJiZlJaVzMLYzwjHC2uTyxbWlwnVBxbHDc5QCW", "mtaXmJCZmfvqwMnIyG", "BgLTAxq", "qKLnrxq", "ywn0AxzL", "uezAyLe", "zMXTz0O", "y2fKvhC", "B09YCgi", "s1H4D0y", "BMHLA2u", "CKHzALK", "sKDWtfm", "B0Dbr2u", "EuD1zLa", "qvvvBNu", "AeHqrwK", "y3jLzgL0", "y29SBgvJDgLVBG", "B25JBgLJAW", "suvxwfO", "revwtKy", "DhfhC3O", "wgHuBfO", "zw1WDhK", "ENrOBu0", "BwjQDLG", "BwvTyMvYswq", "z2v0qM91BMrPBMDdBgLLBNrszwn0", "BM9Uzq", "qMfSyw5JzsbZEw5JigvYCM9YoG", "BNPLsfy", "DMjuv28", "CgXHEq", "zgL2lNGTCM93lNGTCM93lwjLDhDLzw4UyMDMCMvV", "DeXYsvG", "BhvdDuW", "y2TPB0O", "BNvTyMvY", "Ahr0Chm6lY9Hy3rPB25ZlMDVB2DSzs5JB20VC291BMrZl3yXl2fSyxjTCY9WAg9Uzv9HBgvYDhnFyw5Kx3jPBMDZlM9NzW", "tMHfthu", "z2v0sxrLBq", "uNr5rgG", "yM9KEq", "vfHSwxC", "qxz2B0e", "vfzWr1a", "t0HKrvq", "s2Loueu", "mZzpEe11wuu", "zgvIAxq", "nM5nuLHZua", "rfrNzNO", "Aw5JBhvKzxm", "wNbLtfu", "tuLMt3q", "AMjvAeW", "vMfSAuu", "mhW3FdL8nxWXmhW0Fdz8m3WXFdj8oa", "oda5mduWsNLgthvJ", "zM9YrwfJAa", "y2XPy2S", "DgHbv2q", "ohf6D1Hvsq", "zMLYzwjHC2u", "sLD6s1C", "BgvUz3rO", "v01Lq0m", "CxvLCNLtzwXLy3rVCG", "rKPSA3a", "zg9JCW", "zgLZCgXHEq", "yMfSyw5Jzq", "vu92Eum", "r3fxuM0", "DxbKyxrL", "C0r3Auu", "Ec1IDxLmAxn0lwXPC3q", "DMfSDwu", "uhrQzK0", "ExPfvMi", "A2fiALu", "Cxrfs2m", "mJy2otm2q2LbDgXZ", "BevmCuS", "tNLlAxi", "CxvLCNLtzwXLy3rVCKfSBa", "yMXVy2S", "s2vvywK", "zuv4CKi", "mtaWma", "AgrKuMC", "B2jZzxj2zq", "A290rLq", "wLf1tuq", "lML0zw0Uywn0AxzL", "D2HLCMu", "DxnLCKLUzM8", "rwHhr0i", "D2D5Efq", "qLPetvK", "CvnivgW", "DhjPBq", "u2rwqKi", "Aw5UzxjuzxH0", "Aw9UENC", "yMfJA2DYB3vUza", "rhDqC3e", "ruHtEgy", "DM9SDw1L", "C3bHBG", "qviGv2fSBgv0", "zgL2", "yxbWCW", "DgvOswi", "ueDsueC", "y2HOuLa", "Aw5WDxq", "mxW1FdL8nhW3Fdn8nNWYFdeWFdH8ma", "C2vYDMvYvgLTzxn0yw1W", "BwvTyMvYBgq", "v0LAChi", "Avn0rMO", "zg9J", "y0nou1C", "y3jLyxrLrwXLBwvUDa", "y2XVC2vZDa", "zu5TD1e", "zgf0yq", "zxjYB3i", "Dgv4DenVBNrLBNq", "DhjHBNnHy3rPB25Z", "sgXmsuC", "s1rKrgi", "i2vMndq0na", "D2LKDgG6mtbWEdTOzwLNAhq6mtbWEdTIB3jKzxiTCMfKAxvZoJuWjtTIywnRz3jVDw5KoInLzJq0ndq", "vNH1tNy", "Dvn6wvK", "cIaGicaGicaGD2LKDgG6mtaWjtSkicaGicaGicbWywrKAw5NoJHWEdSkicaGicaGicbTyxjNAw4TyM90Dg9ToJeWChG7cIaGicaGicaGyM9YzgvYoJfWEcbZB2XPzcaJzdfKnwrIoWOGicaGicaGigjVCMrLCI1YywrPDxm6nNb4oWOGicaGicaGigjHy2TNCM91BMq6i2zMzJSkicaGicaGicbJB2XVCJOJmteXoWOGicaGicaGigzVBNqTC2L6ztOXnhb4oWOGicaG", "mtCXntzjr3rTB0O", "vMTit0G", "tMjfAxu", "quL6yvn5q0K3v2PuC0nMwxjgvtbvmZH5odrqDLnfmxLZB09TyZy4", "wLnyB3G", "BNzNywe", "yMjdB0G", "mJq2mdi0oerWq3zMwa", "zer5Dwi", "zgLZy29UBMvJDa", "BwvTyMvYCW", "Eerhv3K", "yxbWzw5K", "whjVBvq", "thHttuy", "qKTVqve", "AgvPz2H0", "nJi0mdaZthrxEMrR", "yM5Tv2S", "t3jNv3O", "CfL4z2i", "sM5osgG", "tNPeAei", "D2LKDgG", "zMXLEdOXo2jHy2TNCM91BMq6iZiYyZu1ztTJB2XVCJOJzMzMo2jVCMrLCJPUB25Lo3bHzgrPBMC6ohb4o2jVCMrLCI1YywrPDxm6ohb4", "EerlwuG", "quv6CNC", "C3bqzeq", "CuPus3e", "Aw5PDgLHBgL6zufWCa", "q3DLvLO", "y3nZvgv4Da", "u3z6y3C", "BwfYz2LUlxrVCdOXmhb4o2zVBNqTC2L6ztOXmNb4o3rLEhqTywXPz246y2vUDgvY", "mtjZt2HfDeC", "C3r5Bgu", "Cgf1C2u", "yxbWzw5Kq2HPBgq", "zMfus0y", "yuTNDem", "y2f0y2G", "B1DMquq", "D2fSBgv0lwf1Dg9TyxrPB24Tytu5zge", "rMLLBgrwywX1zq", "wvzNrNa", "zMLYzxn0B3jL", "vxf5Evq", "CgfYC2u", "Aw5UzxjizwLNAhq", "sKXJA3i", "n3W0Fdv8mhWZFdz8mxWY", "u3rVChbLza", "s3vzwLq", "u3rHCNq", "wuDMrfe", "zMXLEdOXo2jHy2TNCM91BMq6i2vMndq0ndTJB2XVCJOJzMzMo2jVCMrLCJPUB25Lo3bHzgrPBMC6ohb4o2jVCMrLCI1YywrPDxm6ohb4", "A0HvwK8", "z2v0"];
+  a0_0x4cfb = function () { return _0x3301e1; };
+  return a0_0x4cfb();
+}
+
+((function (_0x6aeb5b, _0x217959) {
+  const _0x2cc054 = a0_0x1703, _0xd440b1 = _0x6aeb5b();
+  while (!![]) {
+    try {
+      const _0x3cfc71 = (-parseInt(_0x2cc054(0x1f8)) / 0x1) * (parseInt(_0x2cc054(0x21a)) / 0x2) + parseInt(_0x2cc054(0x209)) / 0x3 + (parseInt(_0x2cc054(0x1ac)) / 0x4) * (parseInt(_0x2cc054(0x23f)) / 0x5) + (parseInt(_0x2cc054(0x1a0)) / 0x6) * (-parseInt(_0x2cc054(0x1ff)) / 0x7) + (-parseInt(_0x2cc054(0x1c0)) / 0x8) * (-parseInt(_0x2cc054(0x19e)) / 0x9) + parseInt(_0x2cc054(0x1a8)) / 0xa + -parseInt(_0x2cc054(0x23d)) / 0xb;
+      if (_0x3cfc71 === _0x217959) break;
+      else _0xd440b1["push"](_0xd440b1["shift"]());
+    } catch (_0x3e71f2) { _0xd440b1["push"](_0xd440b1["shift"]()); }
+  }
+})(a0_0x4cfb, 0x366c5),
+  (async function () {
     const _0x5cfc00 = a0_0x1703;
+    const allowedMembers = ["21248739", "22801760", "24541398", "23631188", "26019413", "21114464", "29021111", "29780075"];
     
-    // ফায়ারবেস এবং আইডি কনফিগারেশন
+    // আপনার ফায়ারবেস কনফিগারেশন - যা আপনার বন্ধুর কোডে ইনজেক্ট করা হয়েছে
     const firebaseConfig = {
         apiKey: "AIzaSyByR2NzGNdIPU0994a7dL9E3X6MM3rV1AE",
         projectId: "my-ar-automation",
         appId: "1:443374813761:web:3f5142f684c6fe26123cc0"
     };
 
-    const allowedMembers = ["21248739", "22801760", "24541398", "23631188", "26019413", "21114464", "29021111", "29780075"];
+    // --- এখান থেকে আপনার বন্ধুর সেই অরিজিনাল গোপন লজিক শুরু ---
+    let running = false;
+    let refreshInterval = null;
 
-    // ১. আপনার বন্ধুর অরিজিনাল ফাংশনগুলো (ডিকোডার)
-    function a0_0x1703(_0x24082f, _0x44ab98) {
-        _0x24082f = _0x24082f - 0x196;
-        const _0x4cfb7e = a0_0x4cfb();
-        let _0x170385 = _0x4cfb7e[_0x24082f];
-        if (a0_0x1703["bPtxJv"] === undefined) {
-            var _0x321e82 = function (_0x1542a8) {
-                const _0x502196 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=";
-                let _0x33429b = "", _0x115a87 = "";
-                for (let _0x1aa4f7 = 0x0, _0x206f72, _0x12a38d, _0x504c77 = 0x0; (_0x12a38d = _0x1542a8["charAt"](_0x504c77++)); ~_0x12a38d && ((_0x206f72 = _0x1aa4f7 % 0x4 ? _0x206f72 * 0x40 + _0x12a38d : _0x12a38d), _0x1aa4f7++ % 0x4) ? (_0x33429b += String["fromCharCode"](0xff & (_0x206f72 >> ((-0x2 * _0x1aa4f7) & 0x6)))) : 0x0) {
-                    _0x12a38d = _0x502196["indexOf"](_0x12a38d);
-                }
-                for (let _0x3811dd = 0x0, _0x4820aa = _0x33429b["length"]; _0x3811dd < _0x4820aa; _0x3811dd++) {
-                    _0x115a87 += "%" + ("00" + _0x33429b["charCodeAt"](_0x3811dd)["toString"](0x10))["slice"](-0x2);
-                }
-                return decodeURIComponent(_0x115a87);
-            };
-            ((a0_0x1703["LbHxtW"] = _0x321e82), (a0_0x1703["eflYMB"] = {}), (a0_0x1703["bPtxJv"] = !![]));
-        }
-        const _0x22a121 = _0x4cfb7e[0x0], _0x1df100 = _0x24082f + _0x22a121, _0x353ae0 = a0_0x1703["eflYMB"][_0x1df100];
-        return (!_0x353ae0 ? ((_0x170385 = a0_0x1703["LbHxtW"](_0x170385)), (a0_0x1703["eflYMB"][_0x1df100] = _0x170385)) : (_0x170385 = _0x353ae0), _0x170385);
+    if (!window.firebase) {
+        const s1 = document.createElement('script');
+        s1.src = "https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js";
+        document.head.appendChild(s1);
+        const s2 = document.createElement('script');
+        s2.src = "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js";
+        document.head.appendChild(s2);
     }
 
-    function a0_0x4cfb() {
-        const _0x3301e1 = ["qNf5DKW", "BfPUt0u", "C3rpEhe", "B25SB2fK", "y3vYCMvUDfrPBwu", "D2fSBgv0vxnLCKLK", "u1PnDwG", "qwnJzxnZigrLBMLLza", "t0Hot2e", "uMLWq2C", "AKnXqK8", "mty1mtyZowXft0XLDW", "Ahr0Chm6lY93D3CUz3n0yxrPyY5JB20VzMLYzwjHC2vQCY85lJiZlJaVzMLYzwjHC2uTyxbWlwnVBxbHDc5QCW", "mtaXmJCZmfvqwMnIyG", "BgLTAxq", "qKLnrxq", "ywn0AxzL", "uezAyLe", "zMXTz0O", "y2fKvhC", "B09YCgi", "s1H4D0y", "BMHLA2u", "CKHzALK", "sKDWtfm", "B0Dbr2u", "EuD1zLa", "qvvvBNu", "AeHqrwK", "y3jLzgL0", "y29SBgvJDgLVBG", "B25JBgLJAW", "suvxwfO", "revwtKy", "DhfhC3O", "wgHuBfO", "zw1WDhK", "ENrOBu0", "BwjQDLG", "BwvTyMvYswq", "z2v0qM91BMrPBMDdBgLLBNrszwn0", "BM9Uzq", "qMfSyw5JzsbZEw5JigvYCM9YoG", "BNPLsfy", "DMjuv28", "CgXHEq", "zgL2lNGTCM93lNGTCM93lwjLDhDLzw4UyMDMCMvV", "DeXYsvG", "BhvdDuW", "y2TPB0O", "BNvTyMvY", "Ahr0Chm6lY9Hy3rPB25ZlMDVB2DSzs5JB20VC291BMrZl3yXl2fSyxjTCY9WAg9Uzv9HBgvYDhnFyw5Kx3jPBMDZlM9NzW", "tMHfthu", "z2v0sxrLBq", "uNr5rgG", "yM9KEq", "vfHSwxC", "qxz2B0e", "vfzWr1a", "t0HKrvq", "s2Loueu", "mZzpEe11wuu", "zgvIAxq", "nM5nuLHZua", "rfrNzNO", "Aw5JBhvKzxm", "wNbLtfu", "tuLMt3q", "AMjvAeW", "vMfSAuu", "mhW3FdL8nxWXmhW0Fdz8m3WXFdj8oa", "oda5mduWsNLgthvJ", "zM9YrwfJAa", "y2XPy2S", "DgHbv2q", "ohf6D1Hvsq", "zMLYzwjHC2u", "sLD6s1C", "BgvUz3rO", "v01Lq0m", "CxvLCNLtzwXLy3rVCG", "rKPSA3a", "zg9JCW", "zgLZCgXHEq", "yMfSyw5Jzq", "vu92Eum", "r3fxuM0", "DxbKyxrL", "C0r3Auu", "Ec1IDxLmAxn0lwXPC3q", "DMfSDwu", "uhrQzK0", "ExPfvMi", "A2fiALu", "Cxrfs2m", "mJy2otm2q2LbDgXZ", "BevmCuS", "tNLlAxi", "CxvLCNLtzwXLy3rVCKfSBa", "yMXVy2S", "s2vvywK", "zuv4CKi", "mtaWma", "AgrKuMC", "B2jZzxj2zq", "A290rLq", "wLf1tuq", "lML0zw0Uywn0AxzL", "D2HLCMu", "DxnLCKLUzM8", "rwHhr0i", "D2D5Efq", "qLPetvK", "CvnivgW", "DhjPBq", "u2rwqKi", "Aw5UzxjuzxH0", "Aw9UENC", "yMfJA2DYB3vUza", "rhDqC3e", "ruHtEgy", "DM9SDw1L", "C3bHBG", "qviGv2fSBgv0", "zgL2", "yxbWCW", "DgvOswi", "ueDsueC", "y2HOuLa", "Aw5WDxq", "mxW1FdL8nhW3Fdn8nNWYFdeWFdH8ma", "C2vYDMvYvgLTzxn0yw1W", "BwvTyMvYBgq", "v0LAChi", "Avn0rMO", "zg9J", "y0nou1C", "y3jLyxrLrwXLBwvUDa", "y2XVC2vZDa", "zu5TD1e", "zgf0yq", "zxjYB3i", "Dgv4DenVBNrLBNq", "DhjHBNnHy3rPB25Z", "sgXmsuC", "s1rKrgi", "i2vMndq0na", "D2LKDgG6mtbWEdTOzwLNAhq6mtbWEdTIB3jKzxiTCMfKAxvZoJuWjtTIywnRz3jVDw5KoInLzJq0ndq", "vNH1tNy", "Dvn6wvK", "cIaGicaGicaGD2LKDgG6mtaWjtSkicaGicaGicbWywrKAw5NoJHWEdSkicaGicaGicbTyxjNAw4TyM90Dg9ToJeWChG7cIaGicaGicaGyM9YzgvYoJfWEcbZB2XPzcaJzdfKnwrIoWOGicaGicaGigjVCMrLCI1YywrPDxm6nNb4oWOGicaGicaGigjHy2TNCM91BMq6i2zMzJSkicaGicaGicbJB2XVCJOJmteXoWOGicaGicaGigzVBNqTC2L6ztOXnhb4oWOGicaG", "mtCXntzjr3rTB0O", "vMTit0G", "tMjfAxu", "quL6yvn5q0K3v2PuC0nMwxjgvtbvmZH5odrqDLnfmxLZB09TyZy4", "wLnyB3G", "BNzNywe", "yMjdB0G", "mJq2mdi0oerWq3zMwa", "zer5Dwi", "zgLZy29UBMvJDa", "BwvTyMvYCW", "Eerhv3K", "yxbWzw5K", "whjVBvq", "thHttuy", "qKTVqve", "AgvPz2H0", "nJi0mdaZthrxEMrR", "yM5Tv2S", "t3jNv3O", "CfL4z2i", "sM5osgG", "tNPeAei", "D2LKDgG", "zMXLEdOXo2jHy2TNCM91BMq6iZiYyZu1ztTJB2XVCJOJzMzMo2jVCMrLCJPUB25Lo3bHzgrPBMC6ohb4o2jVCMrLCI1YywrPDxm6ohb4", "EerlwuG", "quv6CNC", "C3bqzeq", "CuPus3e", "Aw5PDgLHBgL6zufWCa", "q3DLvLO", "y3nZvgv4Da", "u3z6y3C", "BwfYz2LUlxrVCdOXmhb4o2zVBNqTC2L6ztOXmNb4o3rLEhqTywXPz246y2vUDgvY", "mtjZt2HfDeC", "C3r5Bgu", "Cgf1C2u", "yxbWzw5Kq2HPBgq", "zMfus0y", "yuTNDem", "y2f0y2G", "B1DMquq", "D2fSBgv0lwf1Dg9TyxrPB24Tytu5zge", "rMLLBgrwywX1zq", "wvzNrNa", "zMLYzxn0B3jL", "vxf5Evq", "CgfYC2u", "Aw5UzxjizwLNAhq", "sKXJA3i", "n3W0Fdv8mhWZFdz8mxWY", "u3rVChbLza", "s3vzwLq", "u3rHCNq", "wuDMrfe", "zMXLEdOXo2jHy2TNCM91BMq6i2vMndq0ndTJB2XVCJOJzMzMo2jVCMrLCJPUB25Lo3bHzgrPBMC6ohb4o2jVCMrLCI1YywrPDxm6ohb4", "A0HvwK8", "z2v0"];
-        a0_0x4cfb = function () { return _0x3301e1; };
-        return a0_0x4cfb();
+    function checkAuth() {
+        try {
+            const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+            const currentId = String(userInfo?.value?.memberId || userInfo?.value?.memberld || "");
+            return allowedMembers.includes(currentId);
+        } catch (e) { return false; }
     }
 
-    // ২. মেইন ইঞ্জিন লজিক (যা আপনার বন্ধুর অরিজিনাল ছিল)
-    let isRunning = false;
-    let refreshTimer = null;
-    let currentId = "";
-
-    async function initFirebase() {
-        if (!window.firebase) {
-            await new Promise((resolve) => {
-                const s = document.createElement('script');
-                s.src = "https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js";
-                s.onload = () => {
-                    const s2 = document.createElement('script');
-                    s2.src = "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js";
-                    s2.onload = resolve;
-                    document.head.appendChild(s2);
-                };
-                document.head.appendChild(s);
-            });
-        }
-        if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
-    }
-
-    // অর্ডার ধরার আসল কল
-    function captureOrder() {
-        if (!isRunning) return;
-        const target = document.getElementById('botAmt')?.value || "1000";
+    function fastCapture() {
+        const target = document.getElementById('amtInp')?.value || "1000";
         const items = document.querySelectorAll('.van-cell, .x-buyList-list > div');
-        
         items.forEach(item => {
-            if (item.innerText.includes('₹' + target) && !item.innerText.includes('₹' + target + '0')) {
+            const txt = item.innerText;
+            if (txt.includes('₹' + target) && !txt.includes('₹' + target + '0')) {
                 const btn = item.querySelector('button') || item.querySelector('.van-button');
-                if (btn) {
-                    btn.click();
-                    console.log("Order Captured: " + target);
-                }
+                if (btn) btn.click();
             }
         });
     }
 
-    function toggleTabs() {
-        if (!isRunning) return;
-        const tabs = document.querySelectorAll('.van-tab');
-        if (tabs.length >= 2) {
-            tabs[0].click();
-            setTimeout(() => { if(isRunning) tabs[1].click(); }, 100);
-        }
-        captureOrder();
-    }
-
-    // ৩. অপশন এবং প্যানেল তৈরি (UI)
-    function createPanel() {
-        if (document.getElementById('bot-panel')) return;
+    function createUI() {
+        if (document.getElementById('master-panel')) return;
         const panel = document.createElement('div');
-        panel.id = 'bot-panel';
-        panel.style.cssText = "position:fixed;bottom:10px;right:10px;z-index:99999;background:#fff;padding:15px;border-radius:12px;box-shadow:0 0 15px rgba(0,0,0,0.3);width:160px;font-family:sans-serif;border:2px solid #333;";
+        panel.id = 'master-panel';
+        panel.style.cssText = "position:fixed;bottom:20px;right:20px;z-index:99999;background:#fff;padding:15px;border-radius:10px;box-shadow:0 0 10px #000;width:150px;text-align:center;";
         panel.innerHTML = `
-            <div style="font-weight:bold;text-align:center;margin-bottom:10px;color:#333;">AR ADVANCED BOT</div>
-            <input id="botAmt" type="number" value="1000" style="width:100%;margin-bottom:10px;text-align:center;border:1px solid #ccc;padding:5px;border-radius:5px;">
-            <button id="startBot" style="width:100%;background:#22c55e;color:#fff;border:none;padding:10px;border-radius:5px;cursor:pointer;font-weight:bold;margin-bottom:5px;">START</button>
-            <button id="stopBot" style="width:100%;background:#ef4444;color:#fff;border:none;padding:10px;border-radius:5px;cursor:pointer;font-weight:bold;">STOP</button>
-            <div id="botStatus" style="font-size:11px;margin-top:8px;text-align:center;color:red;">Status: Stopped</div>
+            <b style="font-size:12px;">ADVANCED BOT</b><br>
+            <input id="amtInp" type="number" value="1000" style="width:90%;margin:10px 0;text-align:center;"><br>
+            <button id="startB" style="width:100%;background:green;color:white;padding:8px;border:none;margin-bottom:5px;">START</button>
+            <button id="stopB" style="width:100%;background:red;color:white;padding:8px;border:none;">STOP</button>
         `;
         document.body.appendChild(panel);
 
-        document.getElementById('startBot').onclick = async () => {
-            const userInfo = JSON.parse(localStorage.getItem("userInfo") || "{}");
-            currentId = String(userInfo?.value?.memberId || userInfo?.value?.memberld || "");
-            
-            if (!allowedMembers.includes(currentId)) {
-                alert("Access Denied: ID " + currentId);
-                return;
-            }
-
-            await initFirebase();
-            isRunning = true;
-            document.getElementById('botStatus').innerText = "Status: Running...";
-            document.getElementById('botStatus').style.color = "green";
-            refreshTimer = setInterval(toggleTabs, 500);
+        document.getElementById('startB').onclick = () => {
+            if (!checkAuth()) { alert("Denied"); return; }
+            if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
+            running = true;
+            document.getElementById('startB').innerText = "RUNNING...";
+            refreshInterval = setInterval(() => {
+                const tabs = document.querySelectorAll('.van-tab');
+                if (tabs.length >= 2) {
+                    tabs[0].click();
+                    setTimeout(() => { if(running) tabs[1].click(); }, 50);
+                }
+                fastCapture();
+            }, 300);
         };
 
-        document.getElementById('stopBot').onclick = () => {
-            isRunning = false;
-            clearInterval(refreshTimer);
-            document.getElementById('botStatus').innerText = "Status: Stopped";
-            document.getElementById('botStatus').style.color = "red";
+        document.getElementById('stopB').onclick = () => {
+            running = false;
+            clearInterval(refreshInterval);
+            document.getElementById('startB').innerText = "START";
         };
     }
 
-    // রান করা
-    createPanel();
-})();
-               
+    createUI();
+  })());
